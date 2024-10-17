e28cac9fb063218d50d4545dc61cffda48188df6
as <as.gamer9205@yandex.com>
2024-10-17 14:13:10 +0300

    add cel

--- src/model/GameBoard.kt
+++ src/model/GameBoard.kt
@@ -3,6 +3,15 @@ import kotlin.Array
 
 class GameBoard {
     private  val  context= Array(3){arrayOfNulls<Symbol>(size = 3)}
+// set cel
+    fun  setCel(row :Int,column: Int,symbol: Symbol){
+        context[row][column]=symbol
+    }
+    // create show content
+
+
+
+
 
 
 
