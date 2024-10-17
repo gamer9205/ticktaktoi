8f9b6ce0fa14b3ca7bb587f314b3cf799d5548e6
as <as.gamer9205@yandex.com>
2024-10-17 21:40:28 +0300

    add elvis op

--- src/model/GameBoard.kt
+++ src/model/GameBoard.kt
@@ -2,13 +2,29 @@ import model.Symbol
 import kotlin.Array
 
 class GameBoard {
-    private  val  context= Array(3){arrayOfNulls<Symbol>(size = 3)}
+    private  val  content= Array(3){arrayOfNulls<Symbol>(size = 3)}
 // set cel
     fun  setCel(row :Int,column: Int,symbol: Symbol){
-        context[row][column]=symbol
+        content[row][column]=symbol
     }
     // create show content
+    fun showContentState(){
+        println("----------")
+       for (i in 0 until MAX_ROW){
+           for (j in 0 until MAX_COLUMN){
+               print(content[i][j] ?: " ")
+           }
+       }
+    }
 
+
+
+
+
+    companion object{
+        const val MAX_ROW=3
+        const val MAX_COLUMN=3
+    }
 
 
 
