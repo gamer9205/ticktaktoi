a4b4ed12dd76f0393bf926acf5e79a2863d381a4
as <as.gamer9205@yandex.com>
2024-10-19 23:01:50 +0300

    updare board

--- src/model/GameBoard.kt
+++ src/model/GameBoard.kt
@@ -14,11 +14,13 @@ class GameBoard {
        for (i in 0 until MAX_ROW){
            for (j in 0 until MAX_COLUMN){
                print(content[i][j] ?: " ")
-               if (j<2)   println("----------")
+               if (j<2)   println("|")
 
            }
-           println("----------")
+           println()
+           if(i<2)("----------")
        }
+        println("----------")
     }
     fun isCellEmpty(row: Int,column:Int )=content[row][column]==null
     fun isWiner( currentPlayer: Player):Boolean{
@@ -27,10 +29,21 @@ class GameBoard {
             if (content[i][0]==symb &&content[i][1]==symb && content[i][2]==symb) {
                 return true
             }
-        }
-        for ()
 
-        return true
+
+        }
+        for (j in 0 until 3){
+            if (content[0][j]==symb &&content[j][1]==symb && content[2][j]==symb) {
+                return true
+            }
+
+
+        }
+        if (content[0][0] == symb && content[1][1] == symb&&content[2][2]==symb) return true
+        if (content[0][2]==symb && content[1][1]==symb&&content[2][0]==symb) return true
+
+
+        return false
     }
 
 
