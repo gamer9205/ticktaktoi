2661a430c87b24b91bd35b3c9c6548f0a067e3f8
as <as.gamer9205@yandex.com>
2024-10-19 23:05:08 +0300

    updare board

--- src/controller/GameController.kt
+++ src/controller/GameController.kt
@@ -1,5 +1,21 @@
 package controller
 
-class GameController {
+import GameBoard
+import model.Player
 
+class GameController {
+private  var player1: Player?=null
+    private  var player2: Player?=null
+    private val gameboard=GameBoard()
+
+    init {
+        initPlayer()
+    }
+
+    private fun initPlayer(){
+        println("Enter the name of Player1")
+        val namePlayer1= readln()
+        val name
+
+    }
 }
