934502f92303bcd5ea6f02264a8131bda44718f6
gamer9205 <as.gamer9205@yandex.com>
2024-10-20 20:47:51 +0300

    add game controler initfunc

--- src/controller/GameController.kt
+++ src/controller/GameController.kt
@@ -2,10 +2,11 @@ package controller
 
 import GameBoard
 import model.Player
+import model.Symbol
 
 class GameController {
-private  var player1: Player?=null
-    private  var player2: Player?=null
+private  lateinit var player1: Player
+ private lateinit var player2: Player
     private val gameboard=GameBoard()
 
     init {
@@ -15,7 +16,17 @@ private  var player1: Player?=null
     private fun initPlayer(){
         println("Enter the name of Player1")
         val namePlayer1= readln()
-        val name
+
+        println("Chose a symbol for Player $namePlayer1 (X or 0)")
+        val symbolfromConsole= readln().uppercase()
+        val symmbol= when(symbolfromConsole){
+            "x","х"->Symbol.x
+            else ->Symbol.o
+        }
+        player1=Player(namePlayer1,symmbol)
+        println("Enter the name of Player 2")
+        val namePlayer2= readln()
+        player2=Player(namePlayer2,if (player1.symbol==Symbol.x)Symbol.o else Symbol.x )
 
     }
 }
